49169132997b75033d041e726db414ea7777ee80
Marijke Drenth <drenth.marijke@googlemail.com>
2021-11-08 19:51:37 +0200

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+student_name = "Marijke"
+student_age = 30
+current_excitement_level = 9001
+student_age_next_year = student_age + 1
+excitement_level_next_week = current_excitement_level + 100
+student_name.upper()
+type(student_name)
+type(student_age)
+
